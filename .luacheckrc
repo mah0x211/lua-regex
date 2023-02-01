095153182b64f5214b667288fbb9b3ca4f6cf4d8
Masatoshi Fukunaga <mah0x211@gmail.com>
2023-02-01 10:16:50 +0900

    Formatting with luaformatter

---
+++ .luacheckrc
@@ -0,0 +1,11 @@
+std = 'max'
+include_files = {
+    'regex.lua',
+    'test/*_test.lua',
+}
+ignore = {
+    'assert',
+    -- unused argument
+    '212',
+}
+
--- regex.lua
+++ regex.lua
@@ -1,279 +1,267 @@
---[[
-
-  Copyright (C) 2017 Masatoshi Teruya
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-
-  regex.lua
-  lua-regex
-  Created by Masatoshi Teruya on 17/06/01.
-
---]]
-
+--
+-- Copyright (C) Masatoshi Fukunaga
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+--
+-- regex.lua
+-- lua-regex
+-- Created by Masatoshi Teruya on 17/06/01.
+--
 --- file scope variables
-local pcre2 = require('pcre2');
-local unpack = unpack or table.unpack;
+local unpack = unpack or table.unpack
+local sub = string.sub
+local type = type
+local pcre2 = require('pcre2')
 --- constants
 local CFLG2OPT_LUT = {
-    i = pcre2.CASELESS,     --: Do caseless matching.
-    s = pcre2.DOTALL,       --: `.` matches anything including NL.
-    m = pcre2.MULTILINE,    --: `^` and `$` match newlines within data.
-    u = pcre2.UTF,          --: Treat pattern and subjects as UTF strings.
-    x = pcre2.EXTENDED,     --: Ignore white space and `#` comments
-};
+    i = pcre2.CASELESS, -- : Do caseless matching.
+    s = pcre2.DOTALL, -- : `.` matches anything including NL.
+    m = pcre2.MULTILINE, -- : `^` and `$` match newlines within data.
+    u = pcre2.UTF, -- : Treat pattern and subjects as UTF strings.
+    x = pcre2.EXTENDED, -- : Ignore white space and `#` comments
+}
 --- static variables
-local RECACHE = setmetatable({},{
-    __mode = 'v'
-});
-
+local RECACHE = setmetatable({}, {
+    __mode = 'v',
+})
 
 --- flgs2opts
--- @param flgs
--- @param lut
--- @return opts
--- @return global
--- @return cache
--- @return jit
-local function flgs2opts( flgs, lut )
-    local opts = {};
-    local global, cache, jit;
+--- @param flgs string
+--- @return table opts
+--- @return boolean global
+--- @return boolean cache
+--- @return boolean jit
+local function flgs2opts(flgs)
+    local opts = {}
+    local global = false
+    local cache = false
+    local jit = false
+    local nopt = 0
 
-    if flgs then
-        local nopt = 0;
+    for i = 1, #flgs do
+        local flg = sub(flgs, i, i)
 
-        assert( type( flgs ) == 'string', 'flgs must be string' );
-        for i = 1, #flgs do
-            local flg = flgs:sub( i, i );
-
+        if flg == 'j' then
             -- jit compile flag
-            if flg == 'j' then
-                jit = true;
+            jit = true
+        elseif flg == 'g' then
             -- global flag
-            elseif flg == 'g' then
-                global = true;
+            global = true
+        elseif flg == 'o' then
             -- compile-once mode flag
-            elseif flg == 'o' then
-                cache = true;
+            cache = true
+        elseif flg == 'U' then
             -- do not check the pattern for UTF valid.
             -- only relevant if UTF option is set.
-            elseif flg == 'U' then
-                opts[nopt + 1] = pcre2.UTF;
-                opts[nopt + 2] = pcre2.NO_UTF_CHECK;
-                nopt = nopt + 2;
-            else
-                local opt = lut[flg];
-
+            opts[nopt + 1] = pcre2.UTF
+            opts[nopt + 2] = pcre2.NO_UTF_CHECK
+            nopt = nopt + 2
+        else
+            local opt = CFLG2OPT_LUT[flg]
+            if not opt then
                 -- invalid flag
-                if not opt then
-                    error( ('unknown flag %q'):format( opt ) );
-                end
-
-                -- add option
-                nopt = nopt + 1;
-                opts[nopt] = opt;
+                error(('unknown flag %q'):format(opt))
             end
+
+            -- add option
+            nopt = nopt + 1
+            opts[nopt] = opt
         end
     end
 
-    return opts, global, cache, jit;
+    return opts, global, cache, jit
 end
 
-
---- class Regex
-local Regex = {};
-
+--- @class Regex
+--- @field p pcre2
+--- @field global boolean
+--- @field lastidx integer
+local Regex = {}
 
 --- matches
--- @param sbj
--- @param offset
--- @return arr
--- @return err
-function Regex:matches( sbj, offset )
-    local head, tail, err = self.p:match_nocap( sbj, offset );
+--- @param sbj string
+--- @param offset integer
+--- @return string[]? arr
+--- @return any err
+function Regex:matches(sbj, offset)
+    local head, tail, err = self.p:match_nocap(sbj, offset)
 
-    if err then
-        return nil, err;
-    -- found
-    else
-        local arr = {};
-        local idx = 1;
+    if head then
+        local arr = {}
+        local idx = 1
 
-        repeat
-            arr[idx] = sbj:sub( head, tail );
-            idx = idx + 1;
+        while head do
+            arr[idx] = sub(sbj, head, tail)
+            idx = idx + 1
+            head, tail, err = self.p:match_nocap(sbj, tail)
+        end
 
-            head, tail, err = self.p:match_nocap( sbj, tail );
-            if err then
-                return nil, err;
-            end
-        until head == nil;
+        if err then
+            return nil, err
+        end
 
-        return arr;
+        return arr
     end
+
+    return nil, err
 end
 
-
 --- match
--- @param sbj
--- @param offset
--- @return arr
--- @return err
-function Regex:match( sbj, offset )
-    local head, tail, err = self.p:match( sbj, offset or self.lastidx );
+--- @param sbj string
+--- @param offset integer?
+--- @return string[]? arr
+--- @return any err
+function Regex:match(sbj, offset)
+    local head, tail, err = self.p:match(sbj, offset or self.lastidx)
 
-    -- found
     if head then
-        local arr = {};
-
+        -- found
+        local arr = {}
         for i = 1, #head do
-            arr[i] = sbj:sub( head[i], tail[i] );
+            arr[i] = sub(sbj, head[i], tail[i])
         end
 
         -- updaet a last-index if global option is enabled
         if self.global == true then
-            self.lastidx = tail[1];
+            self.lastidx = tail[1]
         end
-
-        return arr;
-    -- reset a last-index to 0 if global option is enabled
+        return arr
     elseif self.global then
-        self.lastidx = 0;
+        -- reset a last-index to 0 if global option is enabled
+        self.lastidx = 0
     end
 
-    return nil, err;
+    return nil, err
 end
 
-
 --- indexesof
--- @param sbj
--- @param offset
--- @return heads
--- @return tails
--- @return err
-function Regex:indexesof( sbj, offset )
-    local head, tail, err = self.p:match_nocap( sbj, offset );
+--- @param sbj string
+--- @param offset? integer
+--- @return integer[]? heads
+--- @return integer[]? tails
+--- @return any err
+function Regex:indexesof(sbj, offset)
+    local head, tail, err = self.p:match_nocap(sbj, offset)
 
     -- found
     if head then
-        local heads = {};
-        local tails = {};
-        local idx = 1;
+        local heads = {}
+        local tails = {}
+        local idx = 1
 
-        repeat
-            heads[idx], tails[idx] = head, tail;
-            idx = idx + 1;
+        while head do
+            heads[idx], tails[idx] = head, tail
+            idx = idx + 1
 
-            head, tail, err = self.p:match_nocap( sbj, tail );
-            if err then
-                return nil, nil, err;
-            end
-        until head == nil;
-
-        return heads, tails;
+            head, tail, err = self.p:match_nocap(sbj, tail)
+        end
+        if err then
+            return nil, nil, err
+        end
+        return heads, tails
     end
 
-    return nil, nil, err;
+    return nil, nil, err
 end
 
-
 --- indexof
--- @param sbj
--- @param offset
--- @return heads
--- @return tails
--- @return err
-function Regex:indexof( sbj, offset )
-    local heads, tails, err = self.p:match( sbj, offset or self.lastidx );
+--- @param sbj string
+--- @param offset? integer
+--- @return integer[]? heads
+--- @return integer[]? tails
+--- @return any err
+function Regex:indexof(sbj, offset)
+    local heads, tails, err = self.p:match(sbj, offset or self.lastidx)
 
     -- found
     if heads then
         -- updaet a last-index if global option is enabled
         if self.global == true then
-            self.lastidx = tails[1];
+            self.lastidx = tails[1]
         end
-
-        return heads, tails;
-    -- reset a last-index to 0 if global option is enabled
+        return heads, tails
     elseif self.global then
-        self.lastidx = 0;
+        -- reset a last-index to 0 if global option is enabled
+        self.lastidx = 0
     end
 
-    return nil, nil, err;
+    return nil, nil, err
 end
 
-
 --- test
--- @param sbj
--- @param offset
--- @return ok
--- @return err
-function Regex:test( sbj, offset )
-    local head, tail, err = self.p:match_nocap( sbj, offset or self.lastidx );
+--- @param sbj string
+--- @param offset integer?
+--- @return boolean ok
+--- @return any err
+function Regex:test(sbj, offset)
+    local head, tail, err = self.p:match_nocap(sbj, offset or self.lastidx)
 
     -- found
     if head then
         -- updaet a last-index if global option is enabled
         if self.global == true then
-            self.lastidx = tail;
+            self.lastidx = tail
         end
-
-        return true;
-    -- reset a last-index to 0 if global option is enabled
+        return true
     elseif self.global then
-        self.lastidx = 0;
+        -- reset a last-index to 0 if global option is enabled
+        self.lastidx = 0
     end
 
-    return false, err;
+    return false, err
 end
 
-
 --- new
--- @param pattern
--- @param flgs
--- @return regex
--- @return err
-local function new( pattern, flgs )
-    local opts, global, cache, jit, re;
+--- @param pattern string
+--- @param flgs? string
+--- @return Regex? regex
+--- @return any err
+local function new(pattern, flgs)
+    local re
 
-    assert( type( pattern ) == 'string', 'pattern must be string' );
+    assert(type(pattern) == 'string', 'pattern must be string')
+    if flgs == nil then
+        flgs = ''
+    else
+        assert(type(flgs) == 'string', 'flgs must be string')
+    end
+
     -- parse flags
-    opts, global, cache, jit = flgs2opts( flgs, CFLG2OPT_LUT );
-
+    local cache_key = pattern .. '@' .. flgs
+    local opts, global, cache, jit = flgs2opts(flgs)
     -- check the cache table
     if cache then
-        cache = pattern .. '@' .. flgs;
-        re = RECACHE[cache];
+        re = RECACHE[cache_key]
     end
 
     if not re then
         -- compile
-        local p, err = pcre2.new( pattern, unpack( opts ) );
+        local p, err = pcre2.new(pattern, unpack(opts))
 
         if not p then
-            return nil, err;
-        -- jit compile
+            return nil, err
         elseif jit then
-            local ok;
-
-            ok, err = p:jit_compile();
+            -- jit compile
+            local ok
+            ok, err = p:jit_compile()
             if not ok then
-                return nil, err;
+                return nil, err
             end
         end
 
@@ -281,112 +269,96 @@ local function new( pattern, flgs )
         re = setmetatable({
             p = p,
             global = global,
-            lastidx = 0
+            lastidx = 0,
         }, {
-            __index = Regex
-        });
+            __index = Regex,
+        })
 
         -- save into cache table
         if cache then
-            RECACHE[cache] = re;
+            RECACHE[cache_key] = re
         end
     end
 
-    return re;
+    return re
 end
-
 
 --- matches
--- @param sbj
--- @param pattern
--- @param flgs
--- @param offset
--- @return arr
--- @return err
-local function matches( sbj, pattern, flgs, offset )
-    local re, err = new( pattern, flgs );
-
+--- @param sbj string
+--- @param pattern string
+--- @param flgs? string
+--- @param offset? integer
+--- @return string[]? arr
+--- @return any err
+local function matches(sbj, pattern, flgs, offset)
+    local re, err = new(pattern, flgs)
     if err then
-        return nil, err;
+        return nil, err
     end
-
-    return re:matches( sbj, offset );
+    return re:matches(sbj, offset)
 end
-
 
 --- match
--- @param sbj
--- @param pattern
--- @param flgs
--- @param offset
--- @return arr
--- @return err
-local function match( sbj, pattern, flgs, offset )
-    local re, err = new( pattern, flgs );
-
+--- @param sbj string
+--- @param pattern string
+--- @param flgs? string
+--- @param offset? integer
+--- @return string[]? arr
+--- @return any err
+local function match(sbj, pattern, flgs, offset)
+    local re, err = new(pattern, flgs)
     if err then
-        return nil, err;
+        return nil, err
     end
-
-    return re:match( sbj, offset );
+    return re:match(sbj, offset)
 end
-
 
 --- indexesof
--- @param sbj
--- @param pattern
--- @param flgs
--- @param offset
--- @return heads
--- @return tails
--- @return err
-local function indexesof( sbj, pattern, flgs, offset )
-    local re, err = new( pattern, flgs );
-
+--- @param sbj string
+--- @param pattern string
+--- @param flgs? string
+--- @param offset? integer
+--- @return integer[]? heads
+--- @return integer[]? tails
+--- @return any err
+local function indexesof(sbj, pattern, flgs, offset)
+    local re, err = new(pattern, flgs)
     if err then
-        return nil, err;
+        return nil, err
     end
-
-    return re:indexesof( sbj, offset );
+    return re:indexesof(sbj, offset)
 end
-
 
 --- indexof
--- @param sbj
--- @param pattern
--- @param flgs
--- @param offset
--- @return heads
--- @return tails
--- @return err
-local function indexof( sbj, pattern, flgs, offset )
-    local re, err = new( pattern, flgs );
-
+--- @param sbj string
+--- @param pattern string
+--- @param flgs? string
+--- @param offset? integer
+--- @return integer[]? heads
+--- @return integer[]? tails
+--- @return any err
+local function indexof(sbj, pattern, flgs, offset)
+    local re, err = new(pattern, flgs)
     if err then
-        return nil, err;
+        return nil, err
     end
-
-    return re:indexof( sbj, offset );
+    return re:indexof(sbj, offset)
 end
-
 
 --- test
--- @param sbj
--- @param pattern
--- @param flgs
--- @param offset
--- @return ok
--- @return err
-local function test( sbj, pattern, flgs, offset )
-    local re, err = new( pattern, flgs, offset );
-
+--- @param sbj string
+--- @param pattern string
+--- @param flgs? string
+--- @param offset? integer
+--- @return boolean ok
+--- @return any err
+local function test(sbj, pattern, flgs, offset)
+    local re, err = new(pattern, flgs, offset)
     if err then
-        return nil, err;
+        return nil, err
     end
-
-    return re:test( sbj, offset );
+    return re:test(sbj, offset)
 end
-
 
 return {
     new = new,
@@ -394,5 +366,5 @@ return {
     match = match,
     indexesof = indexesof,
     indexof = indexof,
-    test = test
-};
+    test = test,
+}
